c8ce5db96369034ff80531179368c6044fcfe99a
Alekseyideas <zelenskiy.a@udtech.co>
2020-05-06 12:09:26 +0300

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,48 @@
-import axios from 'axios';
-import Test from './componetns/component';
-import Test2 from './componetns/component2';
+$('document').ready(() => {
+  $('.arg-vid-slider').slick({
+    vertical: true,
+    slidesToShow: 2,
+    infinite: false,
+    arrows: false,
+  });
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-		.then( resp => console.log(resp.data));
+  $('a[data-slide]').click(function (e) {
+    e.preventDefault();
 
-Test();
-Test2();
+    const slideno = $(this).data('slide');
+    // const count = $('a[data-slide]').length;
+    $('.item__badge').removeClass('active');
+    $('.item__desc').removeClass('active');
+    $(this).find('.item__badge').addClass('active');
+    $(this).find('.item__desc').addClass('active');
+    // console.log($('.site .q')[1]);
+    console.log($('.site img').height());
+    switch (+slideno) {
+      case 2:
+        return fnGoTOSlide(50);
+      case 3:
+        return fnGoTOSlide(900);
+      case 4:
+        return fnGoTOSlide(600);
+      case 5:
+        return fnGoTOSlide(1200);
+      case 6:
+        return fnGoTOSlide($('.site img').height());
+      default:
+        return fnGoTOSlide(0);
+    }
+
+    function fnGoTOSlide(n) {
+      // $('.site').animate({ scrollTop: $('.site').scrollTop() + n });
+      console.log();
+      $('.site').animate({
+        scrollTop: n,
+      });
+    }
+
+    // $('.arg-vid-slider').slick(
+    //   'slickGoTo',
+    //   +slideno > 3 ? slideno - 2 : slideno - 1
+    // );
+  });
+});
